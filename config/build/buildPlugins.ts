cf8dd732a813c4b86319c9b617b688f44d4c07dd
Diana Murtazina <murka8831@yandex.ru>
2025-12-21 14:57:38 +0300

    added concurrently, refresh plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import webpack, { DefinePlugin, HotModuleReplacementPlugin } from 'webpack';
 import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export function buildPlugins(
     {paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -25,6 +26,7 @@ export function buildPlugins(
     ]
 
     if(isDev) {
+        plugins.push(new ReactRefreshWebpackPlugin())
         plugins.push(new HotModuleReplacementPlugin())
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false
