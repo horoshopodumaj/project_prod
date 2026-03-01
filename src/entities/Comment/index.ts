a5d94d72f121f7a283718a5ab4a3c1c366944fbc
Diana Murtazina <murka8831@yandex.ru>
2026-03-01 14:08:39 +0300

    dix storybook tests

--- src/entities/Comment/index.ts
+++ src/entities/Comment/index.ts
@@ -1,3 +1,3 @@
-import { CommentList } from "./ui/CommentList/CommentList";
+import CommentList  from "./ui/CommentList/CommentList";
 import { Comment} from './model/types/comment'
 export { CommentList, Comment }
