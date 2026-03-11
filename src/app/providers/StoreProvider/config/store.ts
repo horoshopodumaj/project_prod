b88b76dd0123f811655573b9e48dae6c1a947909
Diana Murtazina <murka8831@yandex.ru>
2026-03-11 19:14:48 +0300

    fix store provider

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -4,13 +4,11 @@ import { counterReducer } from 'entities/Counter'
 import { userReducer } from 'entities/User'
 import { createReducerManager } from './reducerManager'
 import { $api } from 'shared/api/api'
-import { NavigateOptions, To} from 'react-router-dom'
 
 
 export function createReduxStore(
     initialState?: StateSchema, 
     asyncReducers?: ReducersMapObject<StateSchema>,
-    navigate?:  (to: To, options?: NavigateOptions) => void,
 ) {
     const rootReducers : ReducersMapObject<StateSchema> = {
         ...asyncReducers,
@@ -22,7 +20,6 @@ export function createReduxStore(
 
     const extraArg: ThunkExtraArg = {
         api: $api,
-        navigate
     }
 
     const store = configureStore({
