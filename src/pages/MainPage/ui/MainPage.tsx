563c695ff4362c577c1116934e5b5db0b2e3a630
Diana Murtazina <murka8831@yandex.ru>
2026-04-06 15:17:24 +0300

    change Listbox for different lists

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -10,7 +10,16 @@ const MainPage = () => {
         <Page>
             {t("Главная страница")}
             <HStack>
-                <ListBox/>
+                <ListBox 
+                    defaultValue='Выберите значение'
+                    value={undefined}
+                    onChange={console.log}
+                    items={[
+                        {value: '1', content: '123', disabled: true},
+                        {value: '2', content: '213'},
+                        {value: '3', content: '321'},
+                    ]}
+                />
             </HStack>
         </Page>
     )
--- src/shared/ui/ListBox/ListBox.tsx
+++ src/shared/ui/ListBox/ListBox.tsx
@@ -1,59 +1,77 @@
 import { Listbox as HListbox } from '@headlessui/react'
-import { Fragment, useState } from 'react'
+import { Fragment, ReactNode, useState } from 'react'
 import cls from './ListBox.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames'
 import Button from '../Button/Button';
 
-const people = [
-    { id: 1, name: 'Durward Reynolds' },
-    { id: 2, name: 'Kenton Towne' },
-    { id: 3, name: 'Therese Wunsch' },
-    { id: 4, name: 'Benedict Kessler' },
-    { id: 5, name: 'Katelyn Rohan' },
-]
+// const people = [
+//     { id: 1, name: 'Durward Reynolds' },
+//     { id: 2, name: 'Kenton Towne' },
+//     { id: 3, name: 'Therese Wunsch' },
+//     { id: 4, name: 'Benedict Kessler' },
+//     { id: 5, name: 'Katelyn Rohan' },
+// ]
+
+export interface ListBoxItem {
+    value: string;
+    content: ReactNode;
+    disabled?: boolean;
+}
 
 interface ListBoxProps {
     className?: string;
+    items?: ListBoxItem[];
+    value?: string;
+    defaultValue?: string;
+    onChange: (value: string)=> void
 }
 
 export function ListBox(props: ListBoxProps)  {
     const { 
         className, 
+        items,
+        value,
+        defaultValue,
+        onChange
     } = props;
 
-    const [selectedPerson, setSelectedPerson] = useState(people[0])
+    const [selectedPerson, setSelectedPerson] = useState()
 
     return (
         <HListbox 
             as={'div'}
             className={classNames(cls.listBox, {}, [className])}
-            value={selectedPerson} 
-            onChange={setSelectedPerson}
+            value={value} 
+            onChange={onChange}
         >
             <HListbox.Button 
                 className={cls.trigger}
             >
                 <Button>
-                    {selectedPerson.name}
+                    {value ?? defaultValue}
                 </Button>
             </HListbox.Button>
             <HListbox.Options className={cls.options}>
-                {people.map((person) => (
+                {items?.map((item) => (
                     <HListbox.Option 
-                        key={person.id} 
-                        value={person} 
+                        key={item.value} 
+                        value={item.value} 
                         as={Fragment}
+                        disabled={item.disabled}
                     >
                         {({ active, selected }) => (
                             <li
                                 className={classNames(
                                     cls.item, 
-                                    {[cls.active]: active}, 
+                                    {
+                                        [cls.active]: active,
+                                        [cls.disabled]: item.disabled,
+                                    }, 
                                     [className]
                                 )}
                             >
                                 {selected && '!!!'}
-                                {person.name}
+                                {item.content}
                             </li>
                         )}
                     </HListbox.Option>
