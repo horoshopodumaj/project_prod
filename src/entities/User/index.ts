5f7c305b330c8d5b64cf01da1449af5e7045315f
Diana Murtazina <murka8831@yandex.ru>
2026-05-06 14:37:23 +0300

    added use roles

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,6 +1,18 @@
 import { getUserAuthData } from "./model/selectors/getUserAuthData/getUserAuthData";
 import { getUserInited } from "./model/selectors/getUserInited/getUserInited/getUserInited";
+import { isUserAdmin, isUserManager } from "./model/selectors/roleSelector";
 import { userActions, userReducer } from "./model/slice/userSlice";
-import {UserSchema, User} from './model/types/user'
+import {UserSchema, User, UserRole} from './model/types/user'
 
-export { userReducer, userActions, UserSchema, User, getUserAuthData, getUserInited }
+
+
+export { userReducer, 
+    userActions, 
+    UserSchema, 
+    User, 
+    UserRole, 
+    getUserAuthData, 
+    getUserInited, 
+    isUserAdmin, 
+    isUserManager,
+}
