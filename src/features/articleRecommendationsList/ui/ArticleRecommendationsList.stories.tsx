be2e03489ec9e91de398c1375ac4db15aba6b9a0
Diana Murtazina <murka8831@yandex.ru>
2026-05-07 21:51:03 +0300

    added ArticleRecommendationsList story

---
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList.stories.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ArticleRecommendationsList }  from './ArticleRecommendationsList';
+import { StoreDecorators } from 'shared/config/storybook/StoreDecorator/StoreDecorators';
+import withMock from 'storybook-addon-mock';
+import { Article, ArticleType } from 'entities/Article';
+import { ArticleBlockType } from 'entities/Article/model/types/article';
+
+const article: Article = {
+    id: '1',
+    title: 'Javascript news',
+    img: '',
+    subtitle: 'Что нового в JS за 2022 год?',
+    views: 1022,
+    createdAt: '26.02.2022',
+    type: [ArticleType.IT],
+    user: {
+        id: '1',
+        username: 'Ulbi tv',
+    },
+    blocks: [
+        {
+            id: '1',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
+                'JavaScript — это язык, программы на котором можно выполнять в разных средах. В нашем случае речь идёт о браузерах и о серверной платформе Node.js. Если до сих пор вы не написали ни строчки кода на JS и читаете этот текст в браузере, на настольном компьютере, это значит, что вы буквально в считанных секундах от своей первой JavaScript-программы.',
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+        {
+            id: '4',
+            type: ArticleBlockType.CODE,
+            code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
+        },
+        {
+            id: '5',
+            type: ArticleBlockType.TEXT,
+            title: 'Заголовок этого блока',
+            paragraphs: [
+                'Программа, которую по традиции называют «Hello, world!», очень проста. Она выводит куда-либо фразу «Hello, world!», или другую подобную, средствами некоего языка.',
+                'Существуют и другие способы запуска JS-кода в браузере. Так, если говорить об обычном использовании программ на JavaScript, они загружаются в браузер для обеспечения работы веб-страниц. Как правило, код оформляют в виде отдельных файлов с расширением .js, которые подключают к веб-страницам, но программный код можно включать и непосредственно в код страницы. Всё это делается с помощью тега <script>. Когда браузер обнаруживает такой код, он выполняет его. Подробности о теге script можно посмотреть на сайте w3school.com. В частности, рассмотрим пример, демонстрирующий работу с веб-страницей средствами JavaScript, приведённый на этом ресурсе. Этот пример можно запустить и средствами данного ресурса (ищите кнопку Try it Yourself), но мы поступим немного иначе. А именно, создадим в каком-нибудь текстовом редакторе (например — в VS Code или в Notepad++) новый файл, который назовём hello.html, и добавим в него следующий код:',
+            ],
+        },
+    ],
+};
+
+
+export default {
+    title: 'features/ArticleRecommendationsList',
+    component: ArticleRecommendationsList,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    decorators: [withMock]
+} as ComponentMeta<typeof ArticleRecommendationsList>;
+
+const Template: ComponentStory<typeof ArticleRecommendationsList> = (args) => <ArticleRecommendationsList {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
+Primary.decorators = [StoreDecorators({}) ]
+Primary.parameters = {
+    mockData: [
+        {
+            url: `${__API__}/articles?_limit=3`,
+            method: 'GET',
+            status: 200,
+            response: [
+                {...article, id: 1},
+                {...article, id: 2},
+                {...article, id: 3},
+            ],
+        },
+    ],
+};
+
