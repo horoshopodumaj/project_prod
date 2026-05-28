06e00999e2c6f01a1db77b68b15fc88391faeb24
Diana Murtazina <murka8831@yandex.ru>
2026-05-28 19:17:32 +0300

    added script for generate entities

---
+++ scripts/createSlice/templates/schemaTypeTemplate.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const firstCharUpperCase = require('../firstCharUpperCase');
+
+module.exports = (sliceName) => `export interface ${firstCharUpperCase(sliceName)}Schema {
+    
+}`;
